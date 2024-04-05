86847c9ae9a33905aa9c90d3d480e317986addcb
theophilusnuer <tphilus003@gmail.com>
2024-04-05 13:17:10 +0000

    in progress 2

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,14 +6,13 @@ export default function Hero(){
     const [text] = useTypewriter({
       words: ["Full Stack Web Developer.", "Graphic Designer.", "Financial and Data Enthusiast."],
       loop: true,
-      typeSpeed: 20,
+      typeSpeed: 30,
       deleteSpeed: 10,
       delaySpeed: 2000,
     });
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20 ml-10 mr-10">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-white text-lg font-normal">Welcome to Wonderland!</h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">Theophilus Nuer</span>
         </h1>
